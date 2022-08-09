4ee84e41f01e0e7d6d94f73ba54e2bbec98562d6
f646281 <84443306+f646281@users.noreply.github.com>
2022-08-09 15:17:25 +0200

    Update index.php

--- web/index.php
+++ web/index.php
@@ -1,3 +1,10 @@
 <?php
+// the message
+$msg = "First line of text\nSecond line of text";
 
-echo "Hello world";
+// use wordwrap() if lines are longer than 70 characters
+$msg = wordwrap($msg,70);
+
+// send email
+mail("psantos@netquest.com","My subject",$msg);
+?>
